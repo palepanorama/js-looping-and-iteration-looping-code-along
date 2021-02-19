df4ea563a09d1cc8f6ffc8f903c45d492ffb4b0a
palepanorama <matthew.keith.thomas@gmail.com>
2021-02-19 16:46:02 -0600

    commit tree bb902c991a61c1ef89bf1224c706c286ed6e4b94

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    const cards = []
+    for (let i = 0; i < array.length; i++) {
+        cards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards; 
+}
+
+function countDown(num) {
+    while (num > 0) {
+        console.log(num); 
+        num -= 1;
+    }
+    console.log(num); 
+}
